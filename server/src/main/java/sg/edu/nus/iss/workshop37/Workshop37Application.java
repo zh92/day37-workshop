0bb25cc51c5eb5c5dd926c78effff87665aa75f7
zh92 <zhehao92@gmail.com>
2023-05-03 14:41:14 +0800

    Intial commit

---
+++ server/src/main/java/sg/edu/nus/iss/workshop37/Workshop37Application.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.workshop37;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Workshop37Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Workshop37Application.class, args);
+	}
+
+}
---
+++ server/src/test/java/sg/edu/nus/iss/workshop37/Workshop37ApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.workshop37;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Workshop37ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
